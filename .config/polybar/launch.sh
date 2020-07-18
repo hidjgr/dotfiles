425ee3ab4c6a07603f792d8451e3a438fd8ec603
hidjgr <hichemdjeghri1@gmail.com>
2020-07-18 23:50:57 +0100

    updated

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+# Launch Polybar in each monitor, using default config location ~/.config/polybar/config
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo $m > /home/hidjgr/log
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Polybar launched..."
